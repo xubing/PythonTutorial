a21193c0462b686f5687c8e1654e193c2b373bd1
zhangxubing <zhxubing@gmail.com>
2014-09-15 16:10:34 +0800

    add a module demo.how to import and creat a module

---
+++ moduletest.py
@@ -0,0 +1,9 @@
+
+# pythyon -m moduletest
+# in the python shell,import moduletest,and run the moduletest.moduletest(1),you will
+# get 11. ^_^
+
+
+def moduletest(inputvalue):
+    return inputvalue+10;
+    
